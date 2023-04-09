32772d28b1b50f74759284c492ee6095ebc4f2c3
bchan760 <bchan760@gmail.com>
2023-04-08 22:00:32 -0700

    'Update'

--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -12,33 +12,36 @@ function TableHeader()  {
     );
 }
 
-
-
 function TableBody(props) {
     const rows = props.characterData.map((row, index) => {
       return (
         <tr key={index}>
-          <td>{row.name}</td>         
+          <td>{row.name}</td>
           <td>{row.job}</td>
+          <td>
+            <button onClick={() => props.removeCharacter(index)}>Delete</button>
+          </td>
         </tr>
       );
      }
     );
     return (
-        <tbody>
-          {rows}
-         </tbody>
+      <tbody>
+        {rows}
+      </tbody>
     );
 }
 
 
-function Table(props) {
-    return (
+
+
+function Table (props) {
+  return (
     <table>
-        <TableHeader />
-        <TableBody characterData={props.characterData} />
+      <TableHeader />
+      <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
     </table>
-    );
-} 
+  );
+}
 
 export default Table;
